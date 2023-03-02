2f640c660cd56461f19d0eae1f87b2649f2d207e
tatetuke <4619094@ed.tus.ac.jp>
2023-03-02 18:32:26 +0900

    f

--- record.py
+++ record.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     while LABEL == -INF:
         LABLE = int(input())
         print(LABLE)
-        if not (LABEL in np.arraly(0,1,2,3)):
+        if not (LABEL in [0,1,2,3]):
             print("0,1,2,3以外の値が入力されました。")
             print("入力するラベルの値は「0,1,2,3」のいずれかです。")
             LABEL=-INF
